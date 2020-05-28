0181b8b4e75adfd46f3ab62186267bea61ba1223
Anoxiacxy <2819114191@qq.com>
2020-05-29 01:26:33 +0800

    merge branch

--- web design/server/server.cpp
+++ web design/server/server.cpp
@@ -1,83 +1,67 @@
-#include <stdio.h>   
-#include <string.h>         
-#include <stdlib.h>       
+#include <cstdio>   
+#include <cstring>         
+#include <cstdlib>       
 #include <unistd.h>          
 #include <sys/types.h> 
 #include <sys/socket.h> 
 #include <netinet/in.h> 
-#include <arpa/inet.h>
 #include <pthread.h> 
 
 #define MAXBUFF 4096
 
 class Server {
 private:
-    int  listenfd, connfd;
-    struct sockaddr_in  servaddr;
-    char  buff[MAXBUFF];
+    int  server_sockfd;
 public:
-    Server() {
-        if( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
-            printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
-        }
-    }
-    Server () {
-        
+    Server(int MYPORT = 12306, int QUEUESIZE = 10) {
+        server_sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+        struct sockaddr_in server_sockaddr;
+        server_sockaddr.sin_family = AF_INET;
+        server_sockaddr.sin_port = htons(MYPORT);
+        server_sockaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+        socklen_t server_sockaddr = sizeof(server_sockaddr);
+
+        bind(server_sockfd, (struct sockaddr*)&server_sockaddr, server_sockaddr);
+        listen(server_sockfd, QUEUESIZE);
     }
     ~Server() {
-        close(listenfd);
+        close(server_sockfd);
     }
-    run_forever() {
-
+    void getBackendOutput(char * buff) {
+        
+        scanf("%s", buff);
+        printf("\n");
     }
     run_once() {
+        int client_sockfd;
+        struct sockadd_in client_sockaddr;
+        socklen_t client_sockaddr = sizeof(client_sockaddr);
+        char buff[MAXBUFF];
+        
+        client_sockfd = accept(server_sockfd, (struct sockaddr*)client_sockaddr, &client_sockaddr));
+        
+        bool close_flag = false;
 
+        while (!close_flag) {
+            int n = recv(client_sockfd, buff, MAXBUFF, 0);
+            buff[n] = '\0';
+            printf("recv msg from client <<< %s\n", buff);
+            getBackendOutput(buff);
+            printf("send msg to client >>> %s\n", buff);
+            send(client_sockfd, buff, strlen(buff), 0);
+            close_flag = true;    
+        }
+        close(client_sockfd);
     }
-
+    run() {
+        while (run_flag) run_once(); 
+    }
 }
 
-void getBackendOutput(char * buff) {
-    printf("send msg to client >>> ");
-    scanf("%s", buff);
-    printf("\n");
-}
+
 
 int main(int argc, char** argv){
-    
-    
-    
-
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(6666);
-
-    if( bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) == -1){
-        printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);
-        return 0;
-    }
-
-    if( listen(listenfd, 10) == -1){
-        printf("listen socket error: %s(errno: %d)\n",strerror(errno),errno);
-        return 0;
-    }
-
-    printf("======waiting for client's request======\n");
-    while(1){
-        if( (connfd = accept(listenfd, (struct sockaddr*)NULL, NULL)) == -1){
-            printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
-            continue;
-        }
-        n = recv(connfd, buff, MAXLINE, 0);
-        buff[n] = '\0';
-        printf("recv msg from client <<< %s\n", buff);
-        getBackendOutput(buff);
-        
-        if ( send(connfd, buff, strlen(buff), 0) < 0 ) {
-            printf("send socket error: %s(errno: %d)",strerror(errno),errno);
-        }
-        close(connfd);
-    }
     
     return 0;
 }
